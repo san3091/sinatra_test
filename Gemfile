37bf98d50751182617d5748059a5eb04de541964
san3091 <san3091@gmail.com>
2016-02-02 16:16:09 -0500

    added test scraping for apple stock price

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 ruby "2.2.4"
 
 gem "sinatra"
+gem "nokogiri"
+gem "httparty"
--- server.rb
+++ server.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-get '/sinatra' do
-	'Hello World'
+get '/aapl' do
+	response = HTTParty.get("http://finance.yahoo.com/q?s=AAPL")
+  dom = Nokogiri::HTML(response.body)
+  stock_price_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+  appl_stock_price = stock_price_span.text
+  "The current apple stock price is: $#{appl_stock_price.to_s}"
 end
